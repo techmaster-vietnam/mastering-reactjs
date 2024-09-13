b063dc3a1690a1ff093d76dc5a14c352e04cce38
Dung Ta Van <itprono3@gmail.com>
2024-09-13 14:29:07 +0700

    update

--- call-api/client/src/App.tsx
+++ call-api/client/src/App.tsx
@@ -1,26 +1,49 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
+import axios from 'axios';
 
 function App() {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState<any[]>([]);
 
-  function fetchData() {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://localhost:3000/data');
-    xhr.onload = function() {
-      if (xhr.status === 200) {
-        setData(JSON.parse(xhr.responseText));
+  useEffect(() => {
+    const requestInterceptor = axios.interceptors.request.use(
+      (config) => {
+        console.log('Request Intercepted:', config);
+        return config;
       }
+    );
+
+    const responseInterceptor = axios.interceptors.response.use(
+      (response) => {
+        console.log('Response Intercepted:', response);
+        return response;
+      }
+    );
+    return () => {
+      axios.interceptors.request.eject(requestInterceptor);
+      axios.interceptors.response.eject(responseInterceptor);
     };
-    xhr.send();
+  }, []);
+
+  async function fetchData() {
+    try {
+      const response = await axios.get('http://localhost:3000/data');
+      setData(response.data);
+    } catch (error: any) {
+      console.error('Fetch Data Error:', error);
+    }
   }
 
   return (
     <div>
       <button onClick={fetchData}>Get Data</button>
-      {data ? <div>{JSON.stringify(data)}</div> : <div>Loading...</div>}
+      {data.length > 0 ? (
+        <div>{JSON.stringify(data)}</div>
+      ) : (
+        <div>Loading...</div>
+      )}
     </div>
   );
 }
 
-export default App
+export default App;
